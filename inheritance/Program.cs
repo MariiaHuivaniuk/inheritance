31663b7b2a4e56dd6bdc966f7c3ebc2046652978
Masha <huivaniuk.mariia@chnu.edu.ua>
2023-12-04 14:48:21 +0100

    Add project files.

---
+++ inheritance/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Figure
+{
+    public virtual double CalculateArea()
+    {
+        return 0;
+    }
+
+    public virtual double CalculatePerimeter()
+    {
+        return 0;
+    }
+}
+
+class Circle : Figure
+{
+    public double Radius { get; set; }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return 2 * Math.PI * Radius;
+    }
+}
+
+class Rectangle : Figure
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public override double CalculateArea()
+    {
+        return Width * Height;
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return 2 * (Width + Height);
+    }
+}
+      //      Console.WriteLine("Завдання2");
+public class Fruit
+{
+    public string Name { get; set; }
+}
+
+public class Apple : Fruit
+{
+    public string Variety { get; set; }
+    public string Color { get; set; }
+}
+
+public class Pear : Fruit
+{
+    public string Variety { get; set; }
+    public string Color { get; set; }
+}
+
